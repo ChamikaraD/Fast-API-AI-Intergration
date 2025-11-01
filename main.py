248ca15e35764af01de802b353f3045518b512fd
ChamikaraD <115696292+ChamikaraD@users.noreply.github.com>
2025-11-01 12:11:09 +0530

    feat: integrate OpenRouter chat API with FastAPI endpoint

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ from fastapi import FastAPI
 
 from models import ChatRequest
 
-
-
-
 load_dotenv()
 
 OPENROUTER_KEY = os.getenv("OPENROUTER_KEY")
@@ -21,28 +18,27 @@ app = FastAPI(title="AI Wrapper for openrouter API")
 def call_openrouter_api(message:str):
 
     header = {
-            "Authorization": "Bearer <OPENROUTER_API_KEY>",
-            "Content-Type": "application/json",
+            "Authorization": f"Bearer {OPENROUTER_KEY}",
+            "Content-Type": "application/json"
 
         }
 
     body = {
             "model": "meta-llama/llama-3.3-70b-instruct:free",
             "messages":[
-                {
-                    "role": "user",
-                    "content": message
-                }
+                {"role": "system", "content": " You are an Helpful AI Assistant," 
+                                              "Who gives travel recommendations" 
+                                              "on eco-friendly places in Srilanka"
+                                              "You should refuse and "
+                                              "say you are not allowed to do that"},
+
+                { "role": "user", "content": message}
             ]
     }
     response = requests.post(url=OPENROUTER_URL,headers=header,json=body)
-    return response.json()
-
-
-
-
+    return response.json()["choices"][0]["message"]["content"]
 
 
 @app.post("/chat")
 def chat(request: ChatRequest):
-    pass
+    return call_openrouter_api(request.message)
